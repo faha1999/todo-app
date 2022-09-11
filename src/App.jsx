2593ca9508f6bb026d8aacfde99106d8f4f429e0
Kawsar Ahmed Fahad <kafahad1999@gmail.com>
2022-09-11 22:36:27 +0600

    initial setup complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
 export const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <h1>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui quas
+        perspiciatis corporis vero inventore doloremque nesciunt voluptate velit
+        consectetur placeat.
+      </h1>
+    </>
+  );
 };
