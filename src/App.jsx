5af84f93a589716cbc1be625f1ba65825605d6ed
Kawsar Ahmed Fahad <kafahad1999@gmail.com>
2022-09-12 01:57:34 +0600

    added global style

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,7 @@ import React from 'react';
 export const App = () => {
   return (
     <>
-      <h1>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui quas
-        perspiciatis corporis vero inventore doloremque nesciunt voluptate velit
-        consectetur placeat.
-      </h1>
+      <h1></h1>
     </>
   );
 };
